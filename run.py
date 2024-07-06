173d3965b80dc2754949e3c7640e2e1d0f2a007b
ianb <ian246@hotmail.co.uk>
2024-07-06 20:43:30 +0100

    feat: added update_surplus_worksheet() to run.py

--- run.py
+++ run.py
@@ -56,6 +56,15 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print('Sales worksheet updated successfully.\n')
 
+def update_surplus_worksheet(new_surplus):
+    """
+    Update sales worksheet, add new row wth the list data provided
+    """
+    print('Updating surplus worksheet...\n')
+    surplus_worksheet = SHEET.worksheet('surplus')
+    surplus_worksheet.append_row(new_surplus)
+    print('Surplus worksheet updated successfully.\n')
+
 def calculate_surplus(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type
@@ -67,7 +76,13 @@ def calculate_surplus(sales_row):
     print('Calculating surplus data....\n')
     stock = SHEET.worksheet('stock').get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+   
+    surplus_data =[]
+    for stock,sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    
+    return surplus_data
 
 
 def main():
@@ -77,7 +92,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus(sales_data)
+    new_surplus_data = calculate_surplus(sales_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print('Welcome to Love Sandwiches Data Automation')
 main()
